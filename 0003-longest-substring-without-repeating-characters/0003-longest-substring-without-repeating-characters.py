dddd0aeb418934a91d289e0bf929ad9cd714667d
harsh9229913 <66376767+harsh9229913@users.noreply.github.com>
2026-06-29 19:59:36 +0530

    Time: 15 ms (42.73%), Space: 19.3 MB (68.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,12 +3,11 @@ class Solution:
         dup=0
         dct={}
         left=0
-        if s=="":return 0
-        else:
-            i=0
-            for right in range(len(s)):
-                if s[right] in dct and left<=right:
-                   left = max(left, dct[s[right]] + 1)
-                dct[s[right]]=right
-                dup=max(dup,(right-left)+1)
+       
+        i=0
+        for right in range(len(s)):
+            if s[right] in dct and left<=right:
+                left = max(left, dct[s[right]] + 1)
+            dct[s[right]]=right
+            dup=max(dup,(right-left)+1)
         return dup   
